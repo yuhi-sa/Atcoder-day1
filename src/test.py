033275a3ea1f1fb1000ef5477eb48875f374142f
yuhi-sa <tomatomatorl50@gmail.com>
2021-07-11 12:03:56 +0900

    slack内容をリッチに

--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ import slackweb
 import json
 import random
 import os
+import datetime
 
 # 提出結果の取得
 def getSubmissionData(userID):
@@ -49,14 +50,89 @@ def colorFillter(color):
     
     return fillterdID
 
+# slack通知
 def notify(id, color, url):
     slack_api = os.environ['SLACK_API']
     slack = slackweb.Slack(url=slack_api)
-    attachments = [{"title": str(id) + " (" + str(color) + ")" ,
-                "text": url,
-                "color": "good", #good, warning, danger
-                "footer": "Send from Python",
-                }]
+    
+    # 色を和名にする
+    if color =="gray_l":
+        iro = "灰色"
+    elif color == "gray_h":
+        iro = "灰色"
+    elif color == "brown":
+        iro = "茶色"
+    elif color == "green":
+        iro = "緑色"
+    elif color == "skyblue":
+        iro = "水色"
+    elif color == "blue":
+        iro = "青色"
+    elif color == "yellow": 
+        iro = "黄色"
+    elif color == "orange": 
+        iro = "橙色"
+    elif color == "red":
+        iro = "赤色"
+    
+    # 内容
+    attachments = [
+        {
+            "blocks": [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": str(id[0:-2]) + "の" + str(id[-1]) + "問題",
+                    }
+                },
+                {
+                    "type": "section",
+                    "fields": [
+                        		{
+                                    "type": "mrkdwn",
+                                    "text": "*When*"
+                                },
+                                {
+                                    "type": "mrkdwn",
+                                    "text": "*Difficulty*"
+                                },
+                                {
+                                    "type": "plain_text",
+                                    "text": str(datetime.date.today())
+                                },
+                                {
+                                    "type": "plain_text",
+                                    "text": str(iro)
+				}
+                    ]
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "問題へ"
+                            },
+                            "style": "primary",
+                            "url": url
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "設定変更"
+                            },
+                            "url": "https://github.com/yuhi-sa/Atcoder-day1"
+                        }
+                    ]
+                }
+	        ]
+        }
+
+    ]
     slack.notify(text=None, attachments=attachments)
 
 
@@ -80,12 +156,17 @@ def main():
     # 差集合
     unAns = fillterdID - AcceptID
 
-    id = random.choice(list(unAns))
+    # ABCかARCの問題
+    while(True):
+        id = random.choice(list(unAns))
+        if "abc"in id or  "arc" in id:
+            break 
+
     url = "https://atcoder.jp/contests/" + str(id[0:-2]) + "/tasks/" + str(id)
 
-    # print(id, url)
+    print(id, url)
     # slackに通知
-    notify(id, color, url)
+    # notify(id, color, url)
 
 if __name__ == '__main__':
     main()
