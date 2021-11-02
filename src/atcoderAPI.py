ffae23ca15c571322577f7497b4d84d3b4f65bb1
yuhi-sa <tomatomatorl50@gmail.com>
2021-11-02 13:16:24 +0900

    fix comment

--- src/atcoderAPI.py
+++ src/atcoderAPI.py
@@ -8,13 +8,11 @@ EDP_url = "https://kenkoooo.com/atcoder/resources/problem-models.json"
 # Detailed Problems Information
 DPI_url = "https://kenkoooo.com/atcoder/resources/merged-problems.json"
 
-# json形式のデータを取得
 def getJsonData(url):
     response = requests.get(url)
     Data = response.json()
     return Data
 
-# 提出したデータの中でACのものを取得
 def collectAcceptedID(User):
     AcceptID = set()
     for user in User:
@@ -24,39 +22,34 @@ def collectAcceptedID(User):
                 AcceptID.add(data["problem_id"])
     return AcceptID
 
-# 指定色のデータを取得
 def colorFillter(Color):
     Data = getJsonData(EDP_url)
-
     colorCollection  = {
-            "gray_l": (-10000, 0),
-             "gray_h": (0, 400), 
-             "brown_l": (400, 600), 
-             "brown_h": (600, 800),
-             "green_l": (800, 1000),
-             "green_h": (1000, 1200),
-             "skyblue": (1200, 1600), 
-             "blue": (1600, 2000), 
-             "yellow": (2000, 2400), 
-             "orange": (2400, 2800),
-             "red": (2800, 10000)}  
+            "gray_l" : (-10000, 0),
+            "gray_h" : (0, 400), 
+            "brown_l": (400, 600), 
+            "brown_h": (600, 800),
+            "green_l": (800, 1000),
+            "green_h": (1000, 1200),
+            "skyblue": (1200, 1600), 
+            "blue"   : (1600, 2000), 
+            "yellow" : (2000, 2400), 
+            "orange" : (2400, 2800),
+            "red"    : (2800, 10000)
+            }  
     fillterdID = set()
-
     for color in Color:
         for data in Data:
             if "difficulty"in Data[data]:
                 difficulty = Data[data]["difficulty"]
                 if colorCollection[color][0] <= difficulty and colorCollection[color][1] > difficulty:
                     fillterdID.add(data)
-
     return fillterdID
 
-# 問題の難易度を取得
 def getDifficulty(id):
     Data = getJsonData(EDP_url)
     return Data[id]["difficulty"]
 
-# 問題のタイトルを取得
 def getProblemTitle(id):
     Data = getJsonData(DPI_url)
     for data in Data:
@@ -65,7 +58,6 @@ def getProblemTitle(id):
             break
     return title
 
-# ページが存在するかチェック
 def checkPage(url):
     res = requests.get(url)
     if res.status_code == 200:
--- src/test.py
+++ src/test.py
@@ -4,37 +4,32 @@ import atcoderAPI as at
 import slackNotify as sl
 
 def main():
+    # Read User
     f = open('../settings/users.txt', 'r') 
     user = f.read()
     f.close()
     user = user.split(',')
-
+    # Read Color
     f = open('../settings/color.txt', 'r') 
     color = f.read()
     f.close()
     color = color.split(',')
-
-    # だれか一人でもACした問題の集合
-    AcceptID = at.collectAcceptedID(user)
-    # 指定色の問題の集合
+    # Get UnAns Problem
+    AcceptID   = at.collectAcceptedID(user)
     fillterdID = at.colorFillter(color)
-    # 差集合
-    unAns = list(fillterdID - AcceptID)
-
-    slack_api = os.environ['SLACK_API']
-
+    unAns      = list(fillterdID - AcceptID)
+    slack_api  = os.environ['SLACK_API']
+    # Chose and Push
     while(True):
         if len(unAns) == 0:
             sl.errorNotify("誰も解いていない問題がありません．設定を変更してください．", slack_api)
             return
-
-        id = unAns.pop(random.choice(range(len(unAns))))
+        id  = unAns.pop(random.choice(range(len(unAns))))
         url = "https://atcoder.jp/contests/" + str(id[0:-2]) + "/tasks/" + str(id)
         if at.checkPage(url):
             break
-
     difficulty = at.getDifficulty(id)
-    title = at.getProblemTitle(id)
+    title      = at.getProblemTitle(id)
     sl.notify(id, title, difficulty, url, slack_api)
 
 if __name__ == '__main__':
